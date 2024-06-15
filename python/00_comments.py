37c09b3107c7224dea325d24951720d676cffd47
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-06-15 20:42:09 +0100

    comments

---
+++ python/00_comments.py
@@ -0,0 +1,57 @@
+# Welcome to the python tutorial. Is this a tutorial? Idk.
+
+# My name is Athul.
+# For me, this is a project of that I'm building to help you learn the bare fundamentals of
+# python. And, judging by the fact that you're here, I'm guessing you want to try your hand
+# at learning python. That or you're someone looking through my profile, in which case, hi.
+
+# Before we start, I have to tell you that I myself am no python genius, just someone who somewhat
+# knows their way around this highly versatile programming language.
+
+# Regardless, I hope this is helpfull to you on your journey. I'll try my best to explain the
+# bits and pieces that I've found very helpfull. I am someone who believes that you learn best
+# by doing, so this will focus on some problem solving with python to help you better
+# understand how to apply the knowledge I hope to teach you.
+
+# I'll be explaining key concepts in python such as assigning variables, types, functions, etc.
+
+# Google, or whatever site you use to browse the internet, is going to be your best friend
+# when it comes to programming. 
+
+# Now, to get started.
+# First thing on our list is comments.
+# Ironically, comments are us telling the computer that this bit isn't code to be read.
+
+# Now, I already used a few comments, so you might already get it.
+# But, to make a single line comment, you put a "#" in front of the line you want to comment out.
+
+# --------------------------------------------------------------------------- #
+# Make a comment about what you like the most in the world down here:
+
+# --------------------------------------------------------------------------- #
+
+# Yay, you made a single line comment. I sure as hell hope you did cause, if you didn't, I just
+# made myself look stupid. Good job.
+
+# I keep referring to it as a "single line comment", implying that there's more
+# There is, technically. Nope. But let me explain.
+
+"""
+This can be considered a mult line comment. Technically. It works the part for sure. But it isn't
+intended to be such.
+"""
+'''
+This is the same as above. I'm using three instances of quotation marks, single or double, and
+anything sandwiched between them is a long string that is never assigned to a variable and never
+used anywhere in the code and so functions similarly to a comment (will get to strings and
+variables in a quick bit).
+'''
+
+# Officially, there are no other types of comments, other than single line comments, in python.
+# What I did above should not really be used and is just bad practice.
+
+# Final exercise for comments:
+# --------------------------------------------------------------------------- #
+# Give yourself a nickname and write it here:
+
+# --------------------------------------------------------------------------- #
