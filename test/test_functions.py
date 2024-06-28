5baa61f69177925ea62e9b847c07dbdc5002a3cd
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-06-28 10:57:29 +0100

    update tests

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,15 +1,21 @@
+RED = '\033[1;31m'
+GREEN = '\033[1;32m'
+NC = '\033[0m'
+
 def test_subtract_one(result, input):
-    print("Result: {}".format(result))
-    print("Expected Value: {}".format(input-1))
+    COLOR = GREEN if result == input - 1 else RED
+    print("\nExpected Value: {}".format(input-1))
+    print("{}Result: {}".format(COLOR, result))
     if result != (input - 1):
-        print("Incorrect Value. Check your code and try again")
+        print(f"{COLOR}Incorrect Value. Check your code and try again\n{NC}")
     else:
-        print("Correct Value. Good Job.")
+        print(f"{COLOR}Correct Value. Good Job.\n{NC}")
 
 def test_subtract_these(result, num1, num2):
-    print("Result: {}".format(result))
-    print("Expected Value: {}".format(num1 - num2))
+    COLOR = GREEN if result == num1 - num2 else RED
+    print("\nExpected Value: {}".format(num1 - num2))
+    print("{}Result: {}".format(COLOR, result))
     if result != (num1 - num2):
-        print("Incorrect Value. Check your code and try again")
+        print(f"{RED}Incorrect Value. Check your code and try again\n{NC}")
     else:
-        print("Correct Value. Good Job.")
+        print(f"{GREEN}Correct Value. Good Job.\n{NC}")
