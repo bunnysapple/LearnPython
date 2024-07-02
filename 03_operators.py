1c7079558731b06aab659a6c23abf227d027ad25
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-07-02 15:58:28 +0100

    operators

---
+++ 03_operators.py
@@ -0,0 +1,70 @@
+# You've seen two arithmetic operators so far; "+" and "-" operators.
+
+# Here, we'll start to recap on those two operators and learn a bit more about them before we do
+# jump to exercises.
+
+# Let's go through all the main arithmetic operators in python.
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '+' operator ##
+# If you want to see it in action, check 01_functions.py file. I have defined a function that takes
+# in a value and adds one to the value, using this operator. '+' is the addition operator and is used
+# in mathematical operations to add vales together.
+
+# Example:
+print(2 + 3) # This prints the sum of 2 and 3, which is 5.
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '-' operator ##
+# This is the subtraction operator. This operator is used for subtracting values and finding their differences.
+# You've used it in the 02_functions.py file when creating the functions.
+
+# Example:
+print(8 - 3) # This prints the difference between 8 and 3, which is 5
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '*' operator ##
+# '*' is the multiplication operator and  is used for multiplying values.
+
+# Example:
+print(4 * 2) # This prints the result of 4 x 2, which is 8
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '/' operator ##
+# '/' is known as the division operator. It's used in arithmetic functions for dividing numbers.
+
+# Example:
+print(4 / 3) # This calculates the absolute vlalue of this division, which is 1.3333333...
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '%' operator ##
+# '%' is called the modulus operator. This operator is used when you want the remainder when dividing
+# two values.
+
+# Example:
+print(3 % 2) # This prints the value left over once you split 3 of something into pairs of 2, which is 1.
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '//' operator ##
+# '//' operator is called the floor division operator. It is used in floor division, whcich is where you
+# divide to get the closest non-decimal value when rounding down.
+
+# Example:
+print(3 // 2) # This calculates and prints the number of times 2 can fully go into 3, which is one times.
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+## '**' operator ##
+# '**' operator is the exponentiation operator. It's used to find the value of a value to the power of another
+# value.
+
+# Example:
+print(3 ** 2) # This will give you the value for 3², which is 9.
+
+# ---------------------------------------------------------------------------------------------------------- #
