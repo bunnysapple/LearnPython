79eadebb862d3489365b4e764d62fae2e967a26c
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-07-09 20:27:18 +0100

    04_operators.py

---
+++ 04_operators.py
@@ -0,0 +1,88 @@
+from test import Test_Operator
+
+# It's testing time!!
+# Time to put your newly learned operators skills to the test.
+
+# Exercise 1: Addition
+#============================================================================================================#
+# Define a function called add_nums that adds together two numbers and returns the value.
+# Use your knowledge of functions and operators.
+
+
+
+# Uncomment the following two lines before running the code.
+# addition = Test_Operator(add_nums, "add")
+# addition.tests()
+#============================================================================================================#
+
+# Exercise 2: Subtraction
+#============================================================================================================#
+# Define a function called substract_nums that returns the difference between two numbers.
+# Use your knowledge of functions and operators.
+
+
+
+# Uncomment the following two lines before running the code.
+# subtraction = Test_Operator(subtract_nums, "minus")
+# subtraction.tests()
+#============================================================================================================#
+
+# Exercise 3: Multiplication
+#============================================================================================================#
+# Define a function called multiply_nums that returns the multiple of two numbres.
+# Use your knowledge of functions and operators.
+
+
+# Uncomment the following two lines before running the code.
+# multiplication = Test_Operator(multiply_nums, "multiply")
+# multiplication.tests()
+#============================================================================================================#
+
+# Exercise 4: Division
+#============================================================================================================#
+# Define a function called divide_nums that returns the result of dividing the first number with the second.
+# Use your knowledge of functions and operators.
+
+
+# Uncomment the following two lines before running the code.
+# division = Test_Operator(divide_nums, "divide")
+# division.tests()
+#============================================================================================================#
+
+# Exercise 5: Floor Division
+#============================================================================================================#
+# Define a dunction called floor_nums that returns the result of floor dividing two numbers.
+# Use your knowledge of functions and operators.
+
+
+
+# Uncomment the following two lines before running the code.
+# floor_division = Test_Operator(floor_nums, "floor")
+# floor_division.tests()
+#============================================================================================================#
+
+# Exercise 6: Exponentiation
+#============================================================================================================#
+# Define a function called exponentiate_nums which takes two numbers as inputs and returns the first number
+# to the power of the second number.
+# Use your knowledge of functions and operators.
+
+
+
+# Uncomment the following two lines before running the code.
+# exponentiation = Test_Operator(exponentiate_nums, "exponent")
+# exponentiation.tests()
+#============================================================================================================#
+
+# Exercise 7: Modulus
+#============================================================================================================#
+# Define a function called modulus_division which takes two numbers as input and returns the remainder when
+# the first number is divided by the second number
+# Use your knowledge of functions and operators.
+
+
+
+# Uncomment the following two lines before running the code.
+# modulus = Test_Operator(modulus_division, "modulus")
+# modulus.tests()
+#============================================================================================================#
