be9b5337189603063b3930c8244574f7f0eaae06
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-07-11 19:56:10 +0100

    variables

---
+++ 05_variables.py
@@ -0,0 +1,44 @@
+# Now we know how to create functions in python which does certain actions and return results.
+# But, what if we want to store data to be used elsewhere?
+
+# Imaging you have a large paragraph of text that you have to print 5 times in different places.
+# Writing a print function containing those paragraphs 5 times out seems like a waste of space and effort.
+
+# Here's where variables come in. Variables are keywords of sorts that we assign values to.
+# You've probably worked with the concept before in things like maths.
+# For example, if you have a wuestion that asks you to "Find the variable x when 2x = 4", "x" represents
+# a value. In this case it represents 2.
+# Likewise, in programming, you can assign values to key words we cal variables.
+
+# Example:
+hello = "Hello, World!"
+
+# Above I've defined a variable called "hello" and I've assigned the value "Hello, World!" to it.
+# Uncomment the below print statement and run the code to see the result.
+
+# print(hello)
+
+# If you are struggling to run the code, I'd recommend looking back through the files or searching up
+# how to run the code on Google. Google is your best friend when learning and use it wisely.
+
+# Now, hopefully, after running the code, you've seen the phrase "Hello, World!" printed on your terminal.
+# Great. Now, can you try print it multiple times bellow using the print function?
+
+# Exercise:
+# Use the print function to print the variable "hello" 5 times down below.
+
+
+
+# Great job with that. So, let's explain.
+# To define a variable in python, you need to come up with a name.
+# Good practice with naming in python is that you use lower case.
+# If you have a two word name, you seperate the words with "_".
+# Some people also use camel case, "camelCase" instead of "camel_case".
+
+# Once you have a name and wrote it down, you use an equal sign in the middle.
+# This is to equate the variable to what the variable represents.
+
+# After the equal sign, you have what the variable represents.
+# In our case, in the above example, the variable "hello" represents the phrase "Hello, World!"
+
+# Variables can represent a range of things, such as numbers, lists of numbers or phrases, other variables, etc.
